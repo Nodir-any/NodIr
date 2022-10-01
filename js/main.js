7900bbbf096e14e5bea627ddd8793f4ff5013379
Nodir <114328849+Nodir-any@users.noreply.github.com>
2022-10-01 15:13:49 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,50 @@
+var tablinks = document.getElementsByClassName("tab-links");
+		var tabcontents = document.getElementsByClassName("tab-contents");
+		function opentab(tabname){
+			for(tablink of tablinks){
+				tablink.classList.remove("active-link");
+			}
+			for(tabcontent of tabcontents){
+				tabcontent.classList.remove("active-tab");
+			}
+			event.currentTarget.classList.add("active-link");
+			document.getElementById(tabname).classList.add("active-tab");
+		}
+		
+		var sidemenu = document.getElementById("mobile-menu");
+		
+		function openmenu(){
+			sidemenu.style.right = "0";
+		}
+		
+		function closemenu(){
+			sidemenu.style.right = "-200px";
+		}
+		
+		
+//-----------------------------------
+
+document.querySelector('.download-btn').addEventListener('click', function(e){
+	
+	const target = e.target;
+	if(target.matches('button')){
+		target.style.backgroundColor = 'gray';
+		target.style.color = '#cccccc';
+		target.style.fontStyle = 'italic';
+		target.innerText = 'Downloaded';
+	}
+	
+});
+document.querySelector('.github-btn').addEventListener('click', function(e){
+	
+	const target = e.target;
+	if(target.matches('button')){
+		target.style.backgroundColor = 'gray';
+		target.style.color = '#cccccc';
+		target.style.fontStyle = 'italic';
+		target.innerText = 'Went';
+	}
+	
+});
+
+
